122b02fd477669fe04cd556fbbff07839eebef8a
uffizio <uffizio@uffizio-virtual-machine>
2023-02-08 09:53:07 +0530

    1st change

---
+++ test4/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author uffizio
+ *
+ */
+module test4 {
+}
--- test4/src/test4/test1.java
+++ test4/src/test4/test1.java
@@ -2,7 +2,7 @@ package test4;
 
 public class test1 {
 	public static void main(String[] args) {
-		System.out.println("no change");
+		System.out.println("1st change");
 	}
 
 }
